f807276ddf3ed48d1de2e3b1d148230d53c567c1
Madz No <madzno12@gmail.com>
2021-12-08 17:31:26 -0500

    updating rps

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,40 +1,42 @@
-VALID_CHOICES = ["rock", "paper", "scissors"]
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "paper" && second == "rock") ||
+    (first == "scissors" && second == "paper")
+end
+
 def display_result(player, computer)
-  if (player == "rock" && computer == "scissors") ||
-     (player == "paper" && computer == "rock" ) ||
-     (player == "scissors" && computer == "paper")
-  prompt("You won!")
-  elsif (player == "rock" && computer == "paper") ||
-        (player == "paper" && computer == "scissors") ||
-        (player == "scissors" && computer == "rock")
-  prompt("Computer won!")
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
   else
-  prompt("It's a tie!")
+    prompt("It's a tie!")
   end
 end
 
 loop do
   choice = ""
   loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = gets.chomp
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice.")
-  end
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
   end
 
   computer_choice = VALID_CHOICES.sample
   prompt("You chose #{choice}. Computer chose #{computer_choice}.")
 
   display_result(choice, computer_choice)
-  
+
   prompt("Do you want to play again?")
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
