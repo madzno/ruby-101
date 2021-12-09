afc21fe74b312f43b7fcf8a019a414fd9c76168b
Madz No <madzno12@gmail.com>
2021-12-09 13:52:34 -0500

    adding rps bonus features

---
+++ lesson_2/rps_bonus.rb
@@ -0,0 +1,114 @@
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+VALID_NUMBERS = { "1" => "rock",
+                  "2" => "paper",
+                  "3" => "scissors",
+                  "4" => "lizard",
+                  "5" => "spock" }
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def validate_choice?(players_choice)
+  if VALID_NUMBERS.include?(players_choice)
+    players_choice
+  else
+    prompt("That's not a valid choice.")
+  end
+end
+
+def rock_wins?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "rock" && second == "lizard")
+end
+
+def paper_wins?(first, second)
+  (first == "paper" && second == "rock") ||
+    (first == "paper" && second == "spock")
+end
+
+def lizard_wins?(first, second)
+  (first == "lizard" && second == "spock") ||
+    (first == "lizard" && second == "paper")
+end
+
+def spock_wins?(first, second)
+  (first == "spock" && second == "rock") ||
+    (first == "spock" && second == "scissors")
+end
+
+def scissors_wins?(first, second)
+  (first == "scissors" && second == "paper") ||
+    (first == "scissors" && second == "lizard")
+end
+
+def win?(first, second)
+  lizard_wins?(first, second) ||
+    rock_wins?(first, second) ||
+    spock_wins?(first, second) ||
+    scissors_wins?(first, second) ||
+    paper_wins?(first, second)
+end
+
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def display_grand_winner(player_wins, _)
+  if player_wins == 3
+    prompt("You are the grand winner!")
+  else
+    prompt("Computer is the grand winner!")
+  end
+end
+
+player_total = 0
+computer_total = 0
+
+loop do
+  loop do
+    choice = ""
+    loop do
+      choice_prompt = <<-MSG
+      Choose one:
+      1) rock
+      2) paper
+      3) scissors
+      4) lizard
+      5) spock
+      MSG
+      prompt(choice_prompt.to_s)
+      choice = gets.chomp
+      break if choice == validate_choice?(choice)
+    end
+    computer_choice = VALID_CHOICES.sample
+    prompt("You chose #{VALID_NUMBERS[choice]}.
+            Computer chose #{computer_choice}.")
+
+    display_result(VALID_NUMBERS[choice], computer_choice)
+
+    if win?(VALID_NUMBERS[choice], computer_choice)
+      player_total += 1
+    elsif win?(computer_choice, VALID_NUMBERS[choice])
+      computer_total += 1
+    else
+      player_total += 0 && computer_total += 0
+    end
+
+    break if player_total == 3 || computer_total == 3
+  end
+
+  display_grand_winner(player_total, computer_total)
+  prompt("Want to play again? Type 'y' if yes")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt("Thank you for playing! Goodbye")
