2026d6a0c60266c5da2a87d270e2735d004cbf01
Madz No <madzno12@gmail.com>
2021-12-13 11:58:03 -0500

    adding lesson_3 easy 2

---
+++ lesson_3/easy_2_q2.rb
@@ -0,0 +1,10 @@
+munsters_description = "The Munsters are creepy in a good way"
+
+munsters_description.swapcase!
+
+munsters_description.capitalize!
+
+munsters_description.downcase!
+
+munsters_description.upcase!
+
---
+++ lesson_3/easy_2_q5.rb
@@ -0,0 +1,3 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+%w(Fred Barney Wilma Betty BamBam Pebbles)
